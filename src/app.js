b4e63006dd51078494399cf3fedaa6655686156f
Tarun-Nayak <kadavathprakash70@gmail.com>
2022-01-16 22:43:37 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 require("./db/conn");
-const User = require('./models/usermessege');
-const hbs = require('hbs');
+const User = require("./models/usermessege");
+const hbs = require("hbs");
 const { registerPartials } = require("hbs");
 
 const app = express();
@@ -13,12 +13,10 @@ const jspath = path.join(__dirname, "../public");
 const templatepath = path.join(__dirname, "../templates/views");
 const partialpath = path.join(__dirname, "../templates/partials");
 
-
 //middleware
 //app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
 //app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
 //app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
-
 
 //app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,8 +26,7 @@ app.set("view engine", "hbs");
 app.set("views", templatepath);
 hbs.registerPartials(partialpath);
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
