35bf45888a42644c7ab18d6f319785f830cdbd31
Tarun-Nayak <kadavathprakash70@gmail.com>
2022-01-18 13:37:01 +0530

    changes commited

--- src/app.js
+++ src/app.js
@@ -13,12 +13,7 @@ const jspath = path.join(__dirname, "../public");
 const templatepath = path.join(__dirname, "../templates/views");
 const partialpath = path.join(__dirname, "../templates/partials");
 
-//middleware
-//app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
-//app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
-//app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
-
-//app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(staticpath));
 app.use(express.static(jspath));
@@ -27,19 +22,19 @@ app.set("views", templatepath);
 hbs.registerPartials(partialpath);
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
-app.post("/contact", async(req, res) => {
-    try {
-        const userData = new User(req.body);
-        await userData.save();
-        res.status(201).render("index");
-    } catch (error) {
-        res.status(500).send(error);
-    }
+app.post("/contact", async (req, res) => {
+  try {
+    const userData = new User(req.body);
+    await userData.save();
+    res.status(201).render("index");
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`server Started on ${PORT}`);
+  console.log(`server Started on ${PORT}`);
 });
